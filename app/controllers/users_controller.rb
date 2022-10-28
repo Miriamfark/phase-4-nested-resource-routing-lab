eedf292de3e63c495bbd9c76498cbadfd0e71d35
Miriam <miriamjoshua00@gmail.com>
2022-10-27 22:53:57 -0400

    commit tree 85919c328e36d20ca41cbd6758de388e910a15b6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find_by(id: params[:id])
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      items = user.items
+    else
+      user = User.find_by(id: params[:id])
+    end
     render json: user, include: :items
   end
 
